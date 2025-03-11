4d9d682ab25c0fef9b9e9343cac04ff60601f8e7
ThanhLoi64 <thanh.loi@bs.io.vn>
2025-03-11 11:34:53 +0700

    update src code

---
+++ book/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import BookList from "@/components/BookList"
+import BookOverview from "@/components/BookOverview"
+
+export default function Home() {
+  return (
+   <>
+   <BookOverview />
+
+   <BookList />
+   </>
+  )
+}
