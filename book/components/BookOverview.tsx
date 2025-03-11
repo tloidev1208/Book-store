9308993f7fa0fbe10a54cbe8fa3d54e98249b461
yennhitfc <pynhivvk@gmail.com>
2025-03-11 16:20:31 +0700

    hahha

--- book/components/BookOverview.tsx
+++ book/components/BookOverview.tsx
@@ -4,8 +4,7 @@ const BookOverview = () => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
-        <h1>yen nhi ne </h1>
-        <h1>Hong qui nene jbjbjn </h1>
+        <h1>A Long Book title </h1>
       </div>
     </section>
   );
---
+++ book/constants/index.ts
@@ -0,0 +1,18 @@
+export const sampleBooks = [
+{
+    id: 1,
+    title: "The Midnight Book",
+    author: "Matt Haig",
+    genre: "Fantasy / Fiction",
+    rating: 4.6,
+    total_copies: 20,
+    available_copies: 10,
+    description:
+      "A dazzling novel about all the choices that go into a life well lived, The Midnight Library tells the story of Nora Seed as she finds herself between life and death.",
+    color: "#1c1f40",
+    cover: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
+    video: "/sample-video.mp4?updatedAt=1722593504152",
+    summary:
+      "A dazzling novel about all the choices that go into a life well lived, The Midnight Library tells the story of Nora Seed as she finds herself between life and death. A dazzling novel about all the choices that go into a life well lived, The Midnight Library tells the story of Nora Seed as she finds herself between life and death.",
+},
+]
