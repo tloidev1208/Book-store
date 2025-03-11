509507d0b769bd969060df03f0cea8645e4e944d
yennhitfc <pynhivvk@gmail.com>
2025-03-11 15:59:17 +0700

    Update BookOverview.tsx

--- book/components/BookOverview.tsx
+++ book/components/BookOverview.tsx
@@ -4,7 +4,7 @@ const BookOverview = () => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
-        <h1>A Long Book title </h1>
+        <h1>yen nhi ne </h1>
       </div>
     </section>
   );
