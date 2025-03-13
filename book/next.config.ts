39b34947ece3adef39e5e9b9bbd49d532093bef0
zeny <ngthcgiangvvk@gmail.com>
2025-03-13 21:32:05 +0700

    add part 2 home data

--- book/next.config.ts
+++ book/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:"placehol.co",
+      },
+      {
+        protocol:"https",
+        hostname:"m.media-amazon.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
