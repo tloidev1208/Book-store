1f726aac62646f27cce9c09081fd1145cfdfd7cf
yennhitfc <pynhivvk@gmail.com>
2025-03-28 01:25:06 +0700

    fix: authentication

--- book/app/(root)/my-profile/page.tsx
+++ book/app/(root)/my-profile/page.tsx
@@ -1,25 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useTransition } from "react";
 import { Button } from "@/components/ui/button";
-import { signOut } from "@/auth";
+import { signOut } from "next-auth/react"; // Đúng cách
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
 
 const Page = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const handleSignOut = () => {
+    startTransition(async () => {
+      await signOut(); // Đăng xuất đúng cách
+    });
+  };
+
   return (
     <>
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut();
-        }}
-        className="mb-10"
-      >
-        <Button>Đăng xuất</Button>
+      <form action={handleSignOut} className="mb-10">
+        {/* <Button disabled={isPending}>{isPending ? "Đang đăng xuất..." : "Đăng xuất"}</Button> */}
       </form>
 
       <BookList title="Sách mượn" books={sampleBooks} />
     </>
   );
 };
+
 export default Page;
