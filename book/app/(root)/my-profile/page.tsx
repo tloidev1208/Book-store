0dabc007e2be462d0ecfb22d0c4444b600eeeed2
yennhitfc <pynhivvk@gmail.com>
2025-03-31 00:55:45 +0700

    Update page.tsx

--- book/app/(root)/my-profile/page.tsx
+++ book/app/(root)/my-profile/page.tsx
@@ -1,28 +1,25 @@
-"use client";
-
-import React, { useTransition } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react"; // Đúng cách
+import { signOut } from "@/auth";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
 
 const Page = () => {
-  const [isPending, startTransition] = useTransition();
-
-  const handleSignOut = async () => {
-    await signOut({ callbackUrl: "/sign-in" }); // Chuyển hướng về trang đăng nhập sau khi đăng xuất
-  };
-  
-
   return (
     <>
-      <form action={handleSignOut} className="mb-10">
-        <Button disabled={isPending}>{isPending ? "Đang đăng xuất..." : "Đăng xuất"}</Button>
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+        className="mb-10"
+      >
+        <Button>Logout</Button>
       </form>
 
-      <BookList title="Sách mượn" books={sampleBooks} />
+      <BookList title="Borrowed Books" books={sampleBooks} />
     </>
   );
 };
-
 export default Page;
