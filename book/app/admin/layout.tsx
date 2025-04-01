e933ea64fdd25681809b16c33c543464a83d7e45
HongQui185 <Hongqui22003@gmail.com>
2025-04-02 02:05:05 +0700

    fix

--- book/app/admin/layout.tsx
+++ book/app/admin/layout.tsx
@@ -3,7 +3,7 @@ import { auth } from "@/auth";
 import { redirect } from 'next/navigation';
 import '@/styles/admin.css';
 import Sidebar  from '@/components/admin/Sidebar';
-import Header from '@/components/admin/Header';
+import Header from '@/components/Header';
 
 const Layout = async ({children}: { children: ReactNode}) => {
 const session = await auth();
