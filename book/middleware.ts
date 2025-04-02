73dd971fd86d2944945681af7a72fb1a0d426866
yennhitfc <pynhivvk@gmail.com>
2025-04-03 00:17:36 +0700

    Update middleware.ts

--- book/middleware.ts
+++ book/middleware.ts
@@ -5,7 +5,7 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
 
-  // Nếu user chưa đăng nhập và không phải trang /sign-in, chuyển hướng đến /sign-in
+
   if (!token && request.nextUrl.pathname !== "/sign-in") {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
